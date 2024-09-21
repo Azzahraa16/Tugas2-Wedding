37d032a4744baf18ac3549510ed78779db9741c4
Azzahraa16 <araarsy16@gmail.com>
2024-09-21 11:58:30 +0700

    first commit

---
+++ Tugas2/index.php
@@ -0,0 +1,62 @@
+<?php 
+$host_db = "locahost";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
+    <title>The Wedding: Guntur dan Alia</title>
+    <style>
+        body {
+            text-align: center;
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+        }
+        img {
+            max-width: 300px;
+            height: auto;
+            margin-top: 20px;
+        }
+        h1 {
+            font-size: 2.5em;
+            color: #333;
+            margin-top: 20px;
+        }
+        h3{
+            font-family: "Satisfy", cursive;
+            font-size: 30px;
+            margin-bottom: auto;
+        }
+        .links {
+            margin-top: 30px;
+        }
+        a {
+            display: inline-block;
+            margin: 10px;
+            padding: 10px 20px;
+            background-color: #3498db;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+        a:hover {
+            background-color: #2980b9;
+        }
+    </style>
+</head>
+<body>
+<h1>The Wedding</h1>
+<h3>Guntur dan Alia</h3>
+<img src="/pbw/tugas2/images/img.jpg" alt="Mempelai">
+<h2>Getting Married</h2>
+
+<div class="links">
+    <a href="/pbw/tugas2/isibukutamu.php">Isi Buku Tamu</a>
+    <a href="/pbw/tugas2/lihatbukutamu.php">Lihat Buku Tamu</a>
+</div>
+</body>
+</html>
